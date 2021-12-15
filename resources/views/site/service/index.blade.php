d8a1f815d657d225703045a5b36c000f351956cf
LeandroBaroni <leandrobaroni2103@hotmail.com>
2021-12-15 05:12:49 -0300

    Tela base de Servicos

--- resources/views/site/service/index.blade.php
+++ resources/views/site/service/index.blade.php
@@ -1,4 +1,25 @@
 @extends('layouts.site')
-@section('contents')
-página de serviços
-@endsection()
+
+@section('content')
+    <header>
+        <div class="header-wrapper">
+            <h1 class="title-large">Nossos Serviços</h1>
+        </div>
+        <div class="pattern__header"></div>
+    </header>
+    
+    <!- Services List->
+    <div class="gray-background">
+        <section class="products__cultivation__category main-wrapper">
+            <article class="card__product">
+                <div class="card__cover">
+                    
+                </div>
+                <header class="card__product-header">
+                    <h2 class="title-medium">Alinhamento e Balanceamento</h2>
+                    <p>descrição do Serviços</p>
+                </header>
+            </article>
+        </section>
+    </div>
+@endsection
